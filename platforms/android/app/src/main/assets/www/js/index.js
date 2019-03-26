bfe127636146a3b304c981a91730f8a70b3ffb95
Luisfrb56 <luisfrb56@gmail.com>
2019-03-26 16:14:21 +0100

    Primer y ultimo comentario de la camara finalizada

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+   onDeviceReady: function() {
+               navigator.camera.getPicture(this.onSuccess, this.onFail,
+            {
+
+    destinationType: Camera.DestinationType.FILE_URI,
+    popoverOptions: new CameraPopoverOptions(50, 50, 25, 25, Camera.PopoverArrowDirection.ARROW_ANY)
+                });
+    },
+
+   onSuccess:function(imageURI) { 
+     var image = document.getElementById('myImage');
+    image.src = imageURI;
+   }, 
+   
+    onFail:function(message) { 
+      alert('Failed because: ' + message); 
+   }
+};
+
+app.initialize();
